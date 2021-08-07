a5a688bbcc9a80272bed13e1a8416fa0ff870db1
Regina Casias <reginaannvigil@gmail.com>
2021-08-06 19:12:06 -0600

    added service and testing

---
+++ spec/services/map_quest_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'MapQuestService' do
+  it 'returns mapquest service', :vcr do
+    service = MapQuestService.get_location("pueblo", "co")
+    data = service[:results][0][:locations][0]
+
+    expect(data).to have_key(:adminArea5)
+    expect(data[:adminArea5]).to eq("Pueblo")
+    expect(data).to have_key(:adminArea3)
+    expect(data[:adminArea3]).to eq("CO")
+    expect(data).to have_key(:latLng)
+
+    lat_lon = data[:latLng]
+
+    expect(lat_lon).to have_key(:lat)
+    expect(lat_lon[:lat]).to eq(38.265425)
+    expect(lat_lon).to have_key(:lng)
+    expect(lat_lon[:lng]).to eq(-104.610415)
+  end
+end
