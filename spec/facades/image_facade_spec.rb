d60ede11c4365af8c32af52fb2296124a28b2aa1
Regina Casias <reginaannvigil@gmail.com>
2021-08-07 17:18:37 -0600

    Fixed image facade tests

--- spec/facades/image_facade_spec.rb
+++ spec/facades/image_facade_spec.rb
@@ -1,8 +1,14 @@
-# require 'rails_helper'
-#
-# RSpec.describe 'ImageFacade' do
-#   it 'returns MapQuest data correctly', :vcr do
-#     data = UnsplashFacade.location("Pueblo", "Co")
-#
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe 'ImageFacade' do
+  it 'returns Unsplash data correctly', :vcr do
+    data = ImageFacade.new("Pueblo, Co")
+
+    expect(data.credit).to have_key :source
+    expect(data.credit[:source]).to eq("Unsplash.com")
+    expect(data.credit).to have_key :author
+
+    expect(data.image).to have_key :description
+    expect(data.image).to have_key :url
+  end
+end
