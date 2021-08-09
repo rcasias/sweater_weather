807cc7eea74281690034702464d9d31311dfdd17
Regina Casias <reginaannvigil@gmail.com>
2021-08-09 16:04:39 -0600

    Added user register sad path

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -20,10 +20,25 @@ describe "User" do
 
     post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
     created_user = User.last
-    # binding.pry
 
     expect(response).to be_successful
     expect(created_user.email).to eq(user_params[:email])
     expect(created_user.api_key.nil?).to eq(false)
   end
+
+  it "can create a new user sad path password mis match" do
+    user_params = {
+                   email: 'email@email.com',
+                   password: 'password',
+                   password_confirmation: 'passwo'
+                 }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+    created_user = User.last
+
+    expect(response).to_not be_successful
+  end
+
 end
