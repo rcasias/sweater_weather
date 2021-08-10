149e799691f07ae78fdb2dd04044810f9340b4f2
Regina Casias <reginaannvigil@gmail.com>
2021-08-10 09:32:37 -0600

    added service and testing for directions in mapquest

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -3,4 +3,9 @@ class MapQuestService
     response = Faraday.get("http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV['MapQuest_API_KEY']}&location==#{city},#{state}")
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.get_directions(curr_city, curr_state, des_city, des_state)
+    response = Faraday.get("http://www.mapquestapi.com/directions/v2/route?key=#{ENV['MapQuest_API_KEY']}&from=#{curr_city},#{curr_state}&to=#{des_city},#{des_state}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -18,4 +18,14 @@ RSpec.describe 'MapQuestService' do
     expect(lat_lon).to have_key(:lng)
     expect(lat_lon[:lng]).to eq(-104.610415)
   end
+
+  it 'returns mapquest service for directions', :vcr do
+    service = MapQuestService.get_directions("pueblo", "co", "denver", "co")
+    # binding.pry
+    expect(service).to be_a Hash
+
+    route = service[:route]
+
+    expect(route).to have_key :formattedTime
+  end
 end
