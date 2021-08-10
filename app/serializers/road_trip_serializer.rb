b45ef9e482524063cb0c7dc27f523b70c10801a7
Regina Casias <reginaannvigil@gmail.com>
2021-08-10 13:45:24 -0600

    Added sad path testing for raod trip

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,5 +1,6 @@
 class RoadTripSerializer
     include FastJsonapi::ObjectSerializer
+     set_type :roadtrip
     attribute :start_city, :end_city, :travel_time, :weather_at_eta
     set_id { 'null' }
 end
